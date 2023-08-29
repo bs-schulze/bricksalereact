a1f2c5877582f0f1cfa979340f4033aa0910a878
Hannes Schulze <hannes.schulze@byte-solution.net>
2023-08-29 15:29:54 +0200

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import React, { useEffect, useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-import axios from "axios";
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
-import { LegoPrice1 } from "./components/lego-price";
-import { Category, Price } from "./components/types";
-import { CategorySelect } from "./components/category-select";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "./components/layout";
 import { Prices } from "./pages/prices";
 import { Welcome } from "./pages/welcome";
